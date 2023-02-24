50df02718af7dfa489b729313ae20c4d1338cdbb
Evan Hesketh <evanhesketh@yahoo.ca>
2023-02-24 18:46:13 -0500

    first checkpoint

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,20 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+function averagePair(nums, targetAvg) {
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+
+    if (avg === targetAvg) return true;
+
+    if (avg < targetAvg) {
+      left++;
+    } else if (avg > targetAvg) {
+      right--;
+    }
+  }
+  return false;
 }
 
